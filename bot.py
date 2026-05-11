992d50cfd46d1584244cbb2846eb64a93fd1ea5f
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-11 18:42:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,17 @@
 from pyrogram import Client
 from config import *
 
+# BOT CLIENT
 app = Client(
     "PremiumBot",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN
+)
+
+# USERBOT CLIENT
+userbot = Client(
+    "UserbotSession",
+    api_id=API_ID,
+    api_hash=API_HASH
 )
