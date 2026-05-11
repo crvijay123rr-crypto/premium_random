2e83194632f3f21100fa9240721888ecee7b325a
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-11 13:25:36 +0530

    Add files via upload

--- database/mongo.py
+++ database/mongo.py
@@ -1,11 +1,1 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from config import MONGO_URI
-
-mongo = AsyncIOMotorClient(MONGO_URI)
-
-db = mongo["PREMIUM_RANDOM_VIDEO_BOT"]
-
-users = db.users
-videos = db.videos
-plans = db.plans
-stats = db.stats
+# MongoDB connection code
