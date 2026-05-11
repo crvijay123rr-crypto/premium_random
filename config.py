cf328ed6f376d4bfe949c04dc7563eeb6b9037b9
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-12 01:59:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ BOT_TOKEN=os.getenv("BOT_TOKEN")
 MONGO_URI=os.getenv("MONGO_URI")
 OWNER_ID=int(os.getenv("OWNER_ID"))
 LOG_CHANNEL=int(os.getenv("LOG_CHANNEL"))
-FORCE_SUB=os.getenv("FORCE_SUB")
+FORCE_SUB=int(os.getenv("FORCE_SUB"))
 PREMIUM_CHANNEL=int(os.getenv("PREMIUM_CHANNEL"))
 DEMO_CHANNEL=int(os.getenv("DEMO_CHANNEL"))
 
