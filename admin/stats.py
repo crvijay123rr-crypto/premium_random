dd618de503459c9bdab1d32966ae5a7581ca3341
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-11 13:35:29 +0530

    Update stats.py

--- admin/stats.py
+++ admin/stats.py
@@ -1,1 +1,31 @@
-# Stats system
+from pyrogram import filters
+
+from main import app
+from admin.admin import admin_filter
+
+from database.stats_db import (
+    total_users,
+    total_premium_users,
+    total_videos,
+    total_demo_videos
+)
+
+@app.on_message(filters.command("stats") & admin_filter)
+async def stats(client, message):
+
+    users = await total_users()
+    premium = await total_premium_users()
+    videos = await total_videos()
+    demo = await total_demo_videos()
+
+    text = f"""
+📊 BOT STATS
+
+👥 Users : {users}
+👑 Premium : {premium}
+
+🎥 Videos : {videos}
+🎁 Demo Videos : {demo}
+"""
+
+    await message.reply_text(text)# Stats system
