51db7da5c28328f5b76a8b64613c5aad137190e8
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-11 13:18:13 +0530

    Update force_sub.py

--- handlers/force_sub.py
+++ handlers/force_sub.py
@@ -1,1 +1,20 @@
-# Force sub handler
+from pyrogram import filters
+from pyrogram.errors import UserNotParticipant
+
+from main import app
+from config import FORCE_SUB
+
+@app.on_message(filters.private)
+async def force_subscribe(client, message):
+
+    try:
+        user = await app.get_chat_member(
+            FORCE_SUB,
+            message.from_user.id
+        )
+
+    except UserNotParticipant:
+
+        return await message.reply_text(
+            f"⚠️ Join @{FORCE_SUB} First"
+        )# Force sub handler
