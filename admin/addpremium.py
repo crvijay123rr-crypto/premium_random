622e794087454d17c742472b6dba652306430d62
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-11 21:24:05 +0530

    Update addpremium.py

--- admin/addpremium.py
+++ admin/addpremium.py
@@ -1,9 +1,10 @@
 from pyrogram import filters
 
-from main import app
+from bot import app
 from admin.admin import admin_filter
 
 from database.users_db import activate_premium
+
 
 @app.on_message(filters.command("addpremium") & admin_filter)
 async def addpremium(client, message):
@@ -13,11 +14,41 @@ async def addpremium(client, message):
 
     except:
         return await message.reply_text(
-            "/addpremium user_id"
+            "❌ Usage : /addpremium user_id"
         )
 
+    # ACTIVATE PREMIUM
     await activate_premium(user_id)
 
+    # ADMIN REPLY
     await message.reply_text(
-        "✅ Premium Activated"
+        "✅ Premium Activated Successfully"
     )
+
+    # USER MESSAGE
+    try:
+
+        text = f"""
+╔══════════════════╗
+     👑 PREMIUM ACTIVE 👑
+╚══════════════════╝
+
+🎉 Congratulations!
+
+💎 Your Premium Has Been Activated Successfully
+
+⚡ Daily Limit : 100 Videos
+📅 Plan : 30 Days
+🛡 Protected Content Enabled
+
+🔥 Enjoy Unlimited Premium Access
+"""
+
+        await app.send_message(
+            chat_id=user_id,
+            text=text
+        )
+
+    except Exception as e:
+
+        print(f"USER MESSAGE ERROR : {e}")
