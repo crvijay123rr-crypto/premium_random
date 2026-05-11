93bbed4e0d9a6ccbe9e68e951a7cba3f17f130e3
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-11 13:35:46 +0530

    Update ban.py

--- admin/ban.py
+++ admin/ban.py
@@ -1,1 +1,23 @@
-# Ban system
+from pyrogram import filters
+
+from main import app
+from admin.admin import admin_filter
+
+from database.users_db import ban_user
+
+@app.on_message(filters.command("ban") & admin_filter)
+async def ban(client, message):
+
+    try:
+        user_id = int(message.command[1])
+
+    except:
+        return await message.reply_text(
+            "/ban user_id"
+        )
+
+    await ban_user(user_id)
+
+    await message.reply_text(
+        "✅ User Banned"
+    )# Ban system
