0dd1d6ece22f69a536311600222dd36cd510890d
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-12 02:35:56 +0530

    Update adddemo.py

--- admin/adddemo.py
+++ admin/adddemo.py
@@ -5,21 +5,39 @@ from admin.admin import admin_filter
 
 from database.videos_db import add_demo
 
+
 @app.on_message(filters.command("adddemo") & admin_filter)
 async def adddemo(client, message):
 
     reply = message.reply_to_message
 
+    # CHECK REPLY
     if not reply:
+
         return await message.reply_text(
-            "⚠️ Reply To Demo Video"
+            "⚠️ Reply To A Demo Video"
         )
 
+    # ONLY VIDEO ALLOWED
+    if not reply.video:
+
+        return await message.reply_text(
+            "❌ Only Video Messages Allowed"
+        )
+
+    # SAVE DEMO VIDEO
     await add_demo(
         reply.chat.id,
         reply.id
     )
 
+    # SUCCESS MESSAGE
     await message.reply_text(
-        "✅ Demo Video Added"
+        """
+╔════════════════════╗
+    ✅ DEMO ADDED ✅
+╚════════════════════╝
+
+🎁 Demo Video Saved Successfully
+"""
     )
