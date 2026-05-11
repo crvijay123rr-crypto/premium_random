6db75d5a21bc11ef30ce5fc3353a5c992b9a353b
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-12 02:35:21 +0530

    Update addvideo.py

--- admin/addvideo.py
+++ admin/addvideo.py
@@ -5,21 +5,39 @@ from admin.admin import admin_filter
 
 from database.videos_db import add_video
 
+
 @app.on_message(filters.command("addvideo") & admin_filter)
 async def addvideo(client, message):
 
     reply = message.reply_to_message
 
+    # CHECK REPLY
     if not reply:
+
         return await message.reply_text(
-            "⚠️ Reply To Video"
+            "⚠️ Reply To A Video Message"
         )
 
+    # ONLY VIDEO ALLOWED
+    if not reply.video:
+
+        return await message.reply_text(
+            "❌ Only Video Messages Allowed"
+        )
+
+    # SAVE VIDEO
     await add_video(
         reply.chat.id,
         reply.id
     )
 
+    # SUCCESS
     await message.reply_text(
-        "✅ Premium Video Added"
+        """
+╔════════════════════╗
+     ✅ VIDEO ADDED ✅
+╚════════════════════╝
+
+🎬 Premium Video Saved Successfully
+"""
     )
