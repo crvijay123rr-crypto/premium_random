bb358f2ce2f9a6e9ef43f6a7edb469b55802e298
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-11 13:04:45 +0530

    Update buy.py

--- handlers/buy.py
+++ handlers/buy.py
@@ -1,1 +1,23 @@
-# Buy handler
+from pyrogram import filters
+
+from main import app
+from config import PLAN_PRICE
+
+@app.on_message(filters.command("buy"))
+async def buy(client, message):
+
+    text = f"""
+💎 PREMIUM PLAN
+
+📅 Duration : 30 Days
+🎥 Daily Limit : 100 Videos
+
+💰 Price : ₹{PLAN_PRICE}
+
+🟢 UPI Payment:
+upi://pay?pa=yourupi@upi
+
+📤 Send Screenshot After Payment
+"""
+
+    await message.reply_text(text)# Buy handler
