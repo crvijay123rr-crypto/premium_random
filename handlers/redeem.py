fd20e4f54213645ace7c1f4dda03449c4aca530e
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-11 13:17:07 +0530

    Update redeem.py

--- handlers/redeem.py
+++ handlers/redeem.py
@@ -1,1 +1,14 @@
-# Redeem handler
+from pyrogram import filters
+
+from main import app
+
+@app.on_message(filters.command("redeem"))
+async def redeem(client, message):
+
+    text = """
+🎁 REDEEM SYSTEM
+
+Feature Coming Soon
+"""
+
+    await message.reply_text(text)# Redeem handler
