ac4e766ff36a81e9b7df4acb60307d060615ab6c
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-11 13:17:54 +0530

    Update callbacks.py

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -1,1 +1,28 @@
-# Callback handler
+from pyrogram import filters
+
+from main import app
+
+@app.on_callback_query()
+async def callbacks(client, query):
+
+    data = query.data
+
+    if data == "videos":
+        await query.message.reply_text(
+            "/videos"
+        )
+
+    elif data == "demo":
+        await query.message.reply_text(
+            "/demo"
+        )
+
+    elif data == "buy":
+        await query.message.reply_text(
+            "/buy"
+        )
+
+    elif data == "myplan":
+        await query.message.reply_text(
+            "/myplan"
+        )# Callback handler
