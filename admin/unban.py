39447ede91ede6803a7ad7a687409bde37afbeda
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-11 13:36:06 +0530

    Update unban.py

--- admin/unban.py
+++ admin/unban.py
@@ -1,1 +1,23 @@
-# Unban system
+from pyrogram import filters
+
+from main import app
+from admin.admin import admin_filter
+
+from database.users_db import unban_user
+
+@app.on_message(filters.command("unban") & admin_filter)
+async def unban(client, message):
+
+    try:
+        user_id = int(message.command[1])
+
+    except:
+        return await message.reply_text(
+            "/unban user_id"
+        )
+
+    await unban_user(user_id)
+
+    await message.reply_text(
+        "✅ User Unbanned"
+    )# Unban system
