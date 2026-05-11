f17e0d7d9993cf17053fe9f591d6427a8f1e6c72
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-11 13:34:50 +0530

    Create removepremium.py

---
+++ admin/removepremium.py
@@ -0,0 +1,23 @@
+from pyrogram import filters
+
+from main import app
+from admin.admin import admin_filter
+
+from database.users_db import remove_premium
+
+@app.on_message(filters.command("removepremium") & admin_filter)
+async def removepremium(client, message):
+
+    try:
+        user_id = int(message.command[1])
+
+    except:
+        return await message.reply_text(
+            "/removepremium user_id"
+        )
+
+    await remove_premium(user_id)
+
+    await message.reply_text(
+        "✅ Premium Removed"
+    )
