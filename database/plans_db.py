343e2e3f43ccdbea7bbc1f4c76d1cef51c72b03f
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-11 13:27:33 +0530

    Implement CRUD functions for plans in MongoDB

--- database/plans_db.py
+++ database/plans_db.py
@@ -1,1 +1,30 @@
-# Plans DB functions
+from database.mongo import plans
+
+# CREATE PLAN
+async def create_plan(name, price, days, limit):
+
+    await plans.insert_one({
+        "name": name,
+        "price": price,
+        "days": days,
+        "limit": limit
+    })
+
+# GET PLAN
+async def get_plan(name):
+
+    return await plans.find_one({
+        "name": name
+    })
+
+# GET ALL PLANS
+async def get_all_plans():
+
+    return await plans.find().to_list(length=None)
+
+# DELETE PLAN
+async def delete_plan(name):
+
+    await plans.delete_one({
+        "name": name
+    })# Plans DB functions
