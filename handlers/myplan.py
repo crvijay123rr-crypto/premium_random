d3d45a0329a3727f798651a52f84bc284ea7ec54
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-11 17:21:27 +0530

    Update myplan.py

--- handlers/myplan.py
+++ handlers/myplan.py
@@ -2,16 +2,22 @@ from pyrogram import filters
 
 from bot import app
 
-from database.users_db import (
-    get_user
-)
+from database.users_db import get_user
+
 
 @app.on_message(filters.command("myplan"))
 async def myplan(client, message):
 
     user = await get_user(message.from_user.id)
 
-    if not user["premium"]:
+    # USER NOT FOUND
+    if not user:
+        return await message.reply_text(
+            "❌ User not found.\nPlease send /start first."
+        )
+
+    # NO PREMIUM
+    if not user.get("premium"):
         return await message.reply_text(
             "❌ No Active Premium"
         )
@@ -20,10 +26,10 @@ async def myplan(client, message):
 👑 PREMIUM ACTIVE
 
 📅 Expiry :
-{user['expiry']}
+{user.get('expiry', 'Not Set')}
 
 🎥 Used Today :
-{user['used_today']}/100
+{user.get('used_today', 0)}/100
 """
 
-    await message.reply_text(text)# Plan handler
+    await message.reply_text(text)
