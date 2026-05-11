f207e5424ba09fc38955b85d6414bc67792b39c5
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-11 13:47:03 +0530

    Create captions.py

---
+++ utils/captions.py
@@ -0,0 +1,28 @@
+# PREMIUM CAPTION
+def premium_caption(user):
+
+    return f"""
+╔════════════════╗
+   👑 PREMIUM HUB 👑
+╚════════════════╝
+
+👤 User :
+{user.mention}
+
+🆔 ID :
+{user.id}
+
+⚡ Protected Content
+"""
+
+# DEMO CAPTION
+def demo_caption(user):
+
+    return f"""
+🎁 FREE DEMO VIDEO
+
+👤 User :
+{user.mention}
+
+⚠ Auto Delete In 2 Minutes
+"""
