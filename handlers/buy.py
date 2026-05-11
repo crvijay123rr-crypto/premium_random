f87456d2c84495133986eb574363e6f07ebc8885
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-12 01:27:39 +0530

    Update buy.py

--- handlers/buy.py
+++ handlers/buy.py
@@ -3,20 +3,58 @@ from pyrogram import filters
 from bot import app
 from config import PLAN_PRICE
 
+from database.users_db import is_premium
+
+
 @app.on_message(filters.command("buy"))
 async def buy(client, message):
 
+    user_id = message.from_user.id
+
+    # CHECK PREMIUM
+    premium = await is_premium(user_id)
+
+    # ALREADY PREMIUM
+    if premium:
+
+        return await message.reply_text(
+            """
+╔════════════════════╗
+   👑 PREMIUM ACTIVE 👑
+╚════════════════════╝
+
+✅ You Are Already A Premium Member
+
+⚡ Daily Limit : 100 Videos
+🛡 Protected Content Enabled
+
+🔥 Enjoy Unlimited Premium Access
+"""
+        )
+
+    # BUY MESSAGE
     text = f"""
-💎 PREMIUM PLAN
+╔════════════════════╗
+      💎 BUY PREMIUM 💎
+╚════════════════════╝
 
 📅 Duration : 30 Days
 🎥 Daily Limit : 100 Videos
 
+━━━━━━━━━━━━━━━━━━━
+
 💰 Price : ₹{PLAN_PRICE}
 
-🟢 UPI Payment:❌
-only phone pay gift card payment ✅✅
-📤 Send Screenshot After Payment @Contact_45bot
+❌ UPI Payment Not Available
+
+✅ Only PhonePe Gift Card Accepted
+
+📤 Send Payment Screenshot To:
+@Contact_45bot
+
+━━━━━━━━━━━━━━━━━━━
+
+🚀 Get Instant Premium Access
 """
 
-    await message.reply_text(text)# Buy handler
+    await message.reply_text(text)
