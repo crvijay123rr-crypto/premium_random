8cb0395b0ab54dd1e499583a0a10b3361750d9b8
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-11 13:55:24 +0530

    Create verify.py

---
+++ payments/verify.py
@@ -0,0 +1,12 @@
+# PAYMENT VERIFY SYSTEM
+
+async def verify_payment(user_id):
+
+    """
+    MANUAL VERIFY SYSTEM
+
+    Admin checks screenshot
+    Then gives premium
+    """
+
+    return True
