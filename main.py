dc37a252974b8de4c3efce81e39bda64af773c49
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-11 18:15:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from pyrogram import filters
+
 from bot import app
 
+# USER HANDLERS
 import handlers.start
 import handlers.videos
 import handlers.demo
@@ -12,12 +14,30 @@ import handlers.redeem
 import handlers.callbacks
 import handlers.force_sub
 
+# ADMIN HANDLERS
+import admin.addvideo
+import admin.adddemo
+import admin.addpremium
+import admin.broadcast
+import admin.ban
+import admin.unban
+import admin.stats
+
+# SYNC SYSTEM
+import admin.sync_channel
+import admin.sync_demo
+
+
 print("🚀 BOT STARTED SUCCESSFULLY")
 
 
+# PING COMMAND
 @app.on_message(filters.command("ping"))
 async def ping(_, message):
-    await message.reply_text("🏓 Pong!")
+
+    await message.reply_text(
+        "🏓 Pong!"
+    )
 
 
 app.run()
