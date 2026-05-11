a000e506d7256e2b4ff272a849bab1266a30bd5e
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-11 13:37:21 +0530

    Create deletevideo.py

---
+++ admin/deletevideo.py
@@ -0,0 +1,23 @@
+from pyrogram import filters
+
+from main import app
+from admin.admin import admin_filter
+
+from database.videos_db import delete_video
+
+@app.on_message(filters.command("deletevideo") & admin_filter)
+async def deletevideo_cmd(client, message):
+
+    try:
+        msg_id = int(message.command[1])
+
+    except:
+        return await message.reply_text(
+            "/deletevideo msg_id"
+        )
+
+    await delete_video(msg_id)
+
+    await message.reply_text(
+        "🗑 Video Deleted"
+    )
