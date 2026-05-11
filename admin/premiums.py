1a8f5d88c6eb58042d420dbe9049e4311871e3be
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-11 13:37:01 +0530

    Create premiums.py

---
+++ admin/premiums.py
@@ -0,0 +1,17 @@
+from pyrogram import filters
+
+from main import app
+from admin.admin import admin_filter
+
+from database.mongo import users
+
+@app.on_message(filters.command("premiums") & admin_filter)
+async def premiums(client, message):
+
+    total = await users.count_documents({
+        "premium": True
+    })
+
+    await message.reply_text(
+        f"👑 Premium Users : {total}"
+    )
