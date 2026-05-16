16efa30ec9f46d687f8c194bd7e0b21242e9061c
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-16 08:28:22 +0530

    Update premiums.py

--- admin/premiums.py
+++ admin/premiums.py
@@ -5,13 +5,41 @@ from admin.admin import admin_filter
 
 from database.mongo import users
 
+
 @app.on_message(filters.command("premiums") & admin_filter)
 async def premiums(client, message):
+
+    premium_users = users.find({
+        "premium": True
+    })
 
     total = await users.count_documents({
         "premium": True
     })
 
-    await message.reply_text(
-        f"👑 Premium Users : {total}"
-    )
+    text = f"""
+╔════════════════════╗
+      👑 PREMIUM USERS
+╚════════════════════╝
+
+📦 Total Premium Users :
+{total}
+
+━━━━━━━━━━━━━━━━━━━
+"""
+
+    count = 0
+
+    async for user in premium_users:
+
+        count += 1
+
+        user_id = user.get("user_id")
+
+        text += f"\n{count}. `{user_id}`"
+
+    if count == 0:
+
+        text += "\n❌ No Premium Users Found"
+
+    await message.reply_text(text)
