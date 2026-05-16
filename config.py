e041363609bb3d873f70929af922935ba69d685e
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-16 15:10:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,35 @@
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
-API_ID=int(os.getenv("API_ID"))
-API_HASH=os.getenv("API_HASH")
-BOT_TOKEN=os.getenv("BOT_TOKEN")
-MONGO_URI=os.getenv("MONGO_URI")
-OWNER_ID=int(os.getenv("OWNER_ID"))
-LOG_CHANNEL=int(os.getenv("LOG_CHANNEL"))
-FORCE_SUB=os.getenv("FORCE_SUB")
-PREMIUM_CHANNEL=os.getenv("PREMIUM_CHANNEL")
-DEMO_CHANNEL=os.getenv("DEMO_CHANNEL")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-DAILY_LIMIT=1
-DEMO_LIMIT=1
-PLAN_PRICE=125
+MONGO_URI = os.getenv("MONGO_URI")
+
+OWNER_ID = int(os.getenv("OWNER_ID"))
+
+# PREMIUM USERS IDS
+PREMIUM_IDS = [
+    OWNER_ID,
+
+    7842404760,
+    #987654321,
+    #741852963,
+    #852456123,
+    #963258741
+
+    # ADD MORE IDS
+]
+
+LOG_CHANNEL = os.getenv("LOG_CHANNEL")
+
+FORCE_SUB = os.getenv("FORCE_SUB")
+PREMIUM_CHANNEL = os.getenv("PREMIUM_CHANNEL")
+DEMO_CHANNEL = os.getenv("DEMO_CHANNEL")
+
+DAILY_LIMIT = 2
+DEMO_LIMIT = 2
+PLAN_PRICE = 125
